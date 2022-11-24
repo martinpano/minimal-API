6b6aac3bd4741d6451d7f9ea12b99f8e2b62406e
mpanovski <m.panovski@nextech.com>
2022-11-24 12:49:10 +0100

    Helper methods
    Added helper methods for configuring services and middleware

---
+++ MinimalAPI.Demo/MinimalAPI.Demo.Auth/ConfigureMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace MinimalAPI.Demo.Auth
+{
+    public class ConfigureMiddleware
+    {
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.UseAuthentication();
+        }
+    }
+}
---
+++ MinimalAPI.Demo/MinimalAPI.Demo.Auth/ConfigureService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MinimalAPI.Demo.Auth.DataAccess;
+using MinimalAPI.Demo.Auth.Services;
+using System.Text;
+
+namespace MinimalAPI.Demo.Auth
+{
+    public static class ConfigureService
+    {
+        public static void Configure(WebApplicationBuilder builder)
+        {
+            AddSwagerAuth(builder);
+            AddJwtAuthentication(builder);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+
+            RegisterDependencies(builder);
+        }
+        private static void AddSwagerAuth(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Description = "Bearer Authentication with JWT Token",
+                    Type = SecuritySchemeType.Http
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+        private static void AddJwtAuthentication(WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateActor = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+        }
+
+        private static void RegisterDependencies(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<WorldCupDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddTransient<ITeamService, TeamService>();
+            builder.Services.AddTransient<IUserService, UserService>();
+        }
+    }
+}
